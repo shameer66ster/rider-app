48e93d43804d34a9cd1eb2ec17e11cde827dd310
Shameer Ebrahim <shamz@Shameers-MacBook-Air.local>
2025-03-30 21:55:55 +0530

    final change

--- apps/logging-service/src/rider-coordinates/rider-coordinates.module.ts
+++ apps/logging-service/src/rider-coordinates/rider-coordinates.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RiderCoordinatesController } from './rider-coordinates.controller';
 import { RiderCoordinatesService } from './rider-coordinates.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RiderCoordinatesSchema } from './schemas/rider-coordinates.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: RiderCoordinatesSchema.name, schema: RiderCoordinatesSchema }])],
+  
   controllers: [RiderCoordinatesController],
   providers: [RiderCoordinatesService]
 })
